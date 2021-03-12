f5668a71650a293cf2157f5816b73cc65dd26a4b
Sveta <Svetlana.Tolmacheva@appline.ru>
2021-03-13 00:46:21 +0300

    Задание 7 и итоговое 2

--- src/Operators/Itogovaya2.java
+++ src/Operators/Itogovaya2.java
@@ -1,36 +1,50 @@
 package Operators;
-
-
-// есть идеи через массив, но не нахожу информацию как его распарсить
-
 import java.util.Scanner;
 
 public class Itogovaya2 {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-//        String arr[];
-//        System.out.println("Простое уравнение ");
-//        result = str.lastIndexOf('x');
 
-        String a, b, c;
-        System.out.print("a = ");
-        a = in.nextLine();
-        System.out.print("b = ");
-        b = in.nextLine();
-        System.out.print("c = ");
-        c = in.nextLine();
-//
-//        if (a == x)
-//            int x = c - b;
-//        System.out.println("x = " + x);
-//        else if (b == x) {
-//            int x = c - a;
-//            System.out.println("x = " + x);
-//        else(c == x) {
-//                int x = b + a;
-//                System.out.println("x = " + x);
-//            }
-//        }
-    }
-}
+        System.out.println("Простое уравнение ");
+        Scanner console = new Scanner(System.in);
+        String str = console.nextLine();
+        String[] arrSplit = str.split("");
+//        for (String z : arrSplit)
+//            System.out.println(z);
+        String a = arrSplit[0];
+        String b = arrSplit[1];
+        String c = arrSplit[2];
+        String z = arrSplit[4];
+
+        if (a.equals("x") && b.equals("-")) {
+            int i = Integer.parseInt(c);
+            int r = Integer.parseInt(z);
+            int x = r + i;
+            System.out.println("x = " + x);
+        } else if (c.equals("x") && b.equals("-")) {
+            int i = Integer.parseInt(a);
+            int r = Integer.parseInt(z);
+            int x = i - r;
+            System.out.println("x = " + x);
+        } else if (c.equals("x") && b.equals("+")) {
+            int i = Integer.parseInt(a);
+            int r = Integer.parseInt(z);
+            int x = r - i;
+            System.out.println("x = " + x);
+        } else if (z.equals("x") && b.equals("-")) {
+            int i = Integer.parseInt(a.trim());
+            int r = Integer.parseInt(c.trim());
+            int x = i - r;
+            System.out.println("x = " + x);
+        } else if (z.equals("x") && b.equals("+")) {
+            int i = Integer.parseInt(a.trim());
+            int r = Integer.parseInt(c.trim());
+            int x = i + r;
+            System.out.println("x = " + x);
+        } else if (a.equals("x") && b.equals("+")){
+            int i = Integer.parseInt(c.trim());
+            int r = Integer.parseInt(z.trim());
+            int x = r - i;
+            System.out.println("x = " + x);
+        }
+    }}
--- src/Operators/Task7.java
+++ src/Operators/Task7.java
@@ -1,35 +1,40 @@
 package Operators;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Task7 {
-    public static void main(String[] args) {
-        int a = 2;
-        int b = 3;
-        int c = 4;
-        System.out.print("Введите размер массива: ");
-        Scanner console = new Scanner(System.in);
-        int x = console.nextInt();
+    public static void main(String[] args) throws IOException {
 
-        String arr[] = new String[x];
 
-        if (x > 0) {
+                int a = 2;
+                int b = 3;
+                int c = 4;
+                System.out.print("Введите размер массива: ");
+                Scanner console = new Scanner(System.in);
+                int x = console.nextInt();
 
-            System.out.print("Введите значения массива: ");
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = console.nextLine();}
+                console.nextLine();
 
-                if (arr[i].equals(a)) {
-                    System.out.print("Данное значение имеется в константах");
-                } else if (arr[i].equals(b)) {
-                    System.out.print("Данное значение имеется в константах");
-                } else if (arr[i].equals(c)) {
-                    System.out.print("Данное значение имеется в константах");
-                } else {
-                    System.out.print("Такой константы нет!");
+                String arr[] = new String[x];
+
+                if (x > 0) {
+
+                    System.out.print("Введите значения массива: ");
+                    for (int i = 0; i < arr.length; i++) {
+                        arr[i] = console.nextLine();
+
+                        if (arr[i].equals(String.valueOf(a))) {
+                            System.out.print("Данное значение имеется в константах");
+                        } else if (arr[i].equals(String.valueOf(b))) {
+                            System.out.print("Данное значение имеется в константах");
+                        } else if (arr[i].equals(String.valueOf(c))) {
+                            System.out.print("Данное значение имеется в константах");
+                        } else {
+                            System.out.print("Такой константы нет!");
+                        }
+                    }
                 }
-
             }
         }
-    }
 
