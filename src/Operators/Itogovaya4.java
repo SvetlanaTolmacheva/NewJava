0ec505744c6994ba4235266a8a0fd6afb4467714
Sveta <Svetlana.Tolmacheva@appline.ru>
2021-03-13 17:07:51 +0300

    Задание 4

--- src/Operators/Itogovaya4.java
+++ src/Operators/Itogovaya4.java
@@ -4,28 +4,22 @@ import java.util.Scanner;
 
 class Itogovaya4 {
     public static void main (String [] args) {
-        String answer;
-        boolean exit;
-        System.out.println ("Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает?");
-        Scanner inputAnswer = new Scanner (System.in);
 
-        exit = false;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает");
+        String n = scanner.nextLine();
         for(int i = 1; i <= 3; i++){
-            if (exit == true)
-                break;
-            answer = inputAnswer.next ();
-            switch (answer){
-                case ("Заархивированный вирус"):
-                    System.out.println ("Правильно!");
-                    exit = true;
-                    break;
-                case ("Подсказка"):
-                    System.out.println ("Это вирус");
-                    exit = true;
-                    break;
-                default:
-                    System.out.println("Подумай еще.");
-            }
+        if (n.equals("Заархивированный вирус")) {
+            System.out.print("Правильно!");
+            break;
+        } else if (n.equals("Подсказка")){
+            System.out.print("это вирус");
+        } else {
+            System.out.print("Подумай еще!");
+
         }
     }
 }
+    }
+//}
