11c6412c5eda02acbcfd2cf529c1db8e60927b52
Sveta <Svetlana.Tolmacheva@appline.ru>
2021-03-13 17:05:43 +0300

    Итоговые 3 4

--- src/Operators/Itogovaya3.java
+++ src/Operators/Itogovaya3.java
@@ -1,26 +1,76 @@
+
 package Operators;
+
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Itogovaya3 {
-
     public static void main(String[] args) {
         System.out.print("Введите количество строк: ");
         Scanner console = new Scanner(System.in);
         int x = console.nextInt();
-        String arr[] = new String[x];
-        if (x > 0) {
-            System.out.print("Введите значения строк: ");
-            for (int i = 0; i < x; i++) {
-                arr[i] = console.nextLine();
-//            int count = 0;
-//            for (int i = 0; i < x.lenth(); i++) {
-//                count++;
-//
-//               System.out.println("Количество строк: " +x);
-//                System.out.println("Строка 1: " +i);
-//                System.out.println("Ответ ");
+        Scanner scan;
 
+        String text;
+        String resultWord = null;
+        String[] textArray;
+
+        int[] wordsArray;
+        int uniqCount = 0;
+        int minCount = 100;
+        int maxCount = 0;
+
+        System.out.println("Введите текст:");
+
+        scan = new Scanner(System.in);
+        text = scan.nextLine();
+
+        textArray = text.split(" ");
+        wordsArray = new int[textArray.length];
+
+        for(int i = 0; i < textArray.length; i++) {
+            textArray[i] = textArray[i].replaceAll("[“:”,-_\"'`;()]", "");
+            uniqCount = uniqueSymbolsCounter(textArray[i]);
+            wordsArray[i] = uniqCount;
+        }
+
+        for(int i = 0; i < wordsArray.length; i++) {
+            if(maxCount < wordsArray[i]) {
+                maxCount = wordsArray[i];
             }
         }
+
+        for(int i = 0; i < wordsArray.length; i++) {
+            if(maxCount == wordsArray[i]) {
+                if(minCount < textArray[i].length()) {
+                    resultWord = textArray[i];
+                    minCount = textArray[i].length();
+                }
+            }
+        }
+        System.out.println("Количество строк: " + x);
+        System.out.println("Ответ:\n" + resultWord);
+    }
+    private static int uniqueSymbolsCounter(String word) {
+        ArrayList <Character> lst = new ArrayList<Character>();
+        char[] chars = word.toCharArray();
+
+        int count = 0;
+
+        for(char c : chars) {
+            if (!lst.contains(c)) {
+                lst.add(c);
+            }
+        }
+
+        count = lst.size();
+        return count;
     }
 }
+
+//        System.out.print("Введите количество строк: ");
+//        Scanner console = new Scanner(System.in);
+//        int x = console.nextInt();
+//          if (x > 0) {
+//
--- src/Operators/Itogovaya4.java
+++ src/Operators/Itogovaya4.java
@@ -1,26 +1,31 @@
+
 package Operators;
-import java.text.BreakIterator;
 import java.util.Scanner;
 
-public class Itogovaya4 {
+class Itogovaya4 {
+    public static void main (String [] args) {
+        String answer;
+        boolean exit;
+        System.out.println ("Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает?");
+        Scanner inputAnswer = new Scanner (System.in);
 
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-            System.out.println("Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает");
-            String n = scanner.nextLine();
-//        int counter = 3;
-//        while (counter < 3) {
-                if (n.equals("Заархивированный вирус")) {
-                    System.out.print("Правильно!");
-                } else if (n.equals("Подсказка")){
-                    System.out.print("это вирус");
-                    } else {
-                    System.out.print("Подумай еще!");
-
-                }
+        exit = false;
+        for(int i = 1; i <= 3; i++){
+            if (exit == true)
+                break;
+            answer = inputAnswer.next ();
+            switch (answer){
+                case ("Заархивированный вирус"):
+                    System.out.println ("Правильно!");
+                    exit = true;
+                    break;
+                case ("Подсказка"):
+                    System.out.println ("Это вирус");
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Подумай еще.");
             }
         }
-//    }
-//}
+    }
+}
